9fa7c5d6b8af2106455fea656dd2fa82a4389670
harishkandekar <harish.kandekar@ctiotsolution.com>
2023-10-19 16:05:20 +0530

    Added code fpr google sign in

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hsba_app/main.dart';
+import 'package:hsba_app/sign_in_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( SignInScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
