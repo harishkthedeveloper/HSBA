11937bb71011bdc0eac164a037d5651802a563d4
harishkandekar <harish.kandekar@ctiotsolution.com>
2023-10-20 11:55:26 +0530

    assets added

--- lib/google_sign_in_button.dart
+++ lib/google_sign_in_button.dart
@@ -16,7 +16,7 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
       child: _isSigningIn
-          ? CircularProgressIndicator(
+          ? const CircularProgressIndicator(
         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
       )
           : OutlinedButton(
@@ -51,8 +51,8 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
             }
           }
 ,
-          child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: const Padding(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
           child: Row(
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -62,7 +62,7 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
                 height: 35.0,
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 10),
+                padding: EdgeInsets.only(left: 10),
                 child: Text(
                   'Sign in with Google',
                   style: TextStyle(
